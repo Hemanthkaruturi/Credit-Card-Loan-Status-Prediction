9004f9b19b69ac3c3e9148045d61dc6cbd37a42c
Hemanth <karuturihemanth@gmail.com>
2018-03-21 14:53:32 +0530

    Update Credit Loan Status Prediction.R

--- Credit Loan Status Prediction.R
+++ Credit Loan Status Prediction.R
@@ -2,7 +2,7 @@
 
 library(data.table)
 train <- fread(file.choose(), stringsAsFactors = T)
-test <- fread(file.choose(), stringsAsFactors = T)
+#test <- fread(file.choose(), stringsAsFactors = T)
 
 #Deleting Empty rows
 train <- head(train,100000)
@@ -12,7 +12,7 @@ train <- subset(train, !duplicated(`Loan ID`))
 
 #Formatting Years in current job column
 levels(train$`Years in current job`)[levels(train$`Years in current job`) ==  "8 years"] <- "8"
-levels(train$`Years in current job`)[levels(train$`Years in current job`) ==  "10+ years"] <- "8"
+levels(train$`Years in current job`)[levels(train$`Years in current job`) ==  "10+ years"] <- "11"
 levels(train$`Years in current job`)[levels(train$`Years in current job`) ==  "3 years"] <- "3"
 levels(train$`Years in current job`)[levels(train$`Years in current job`) ==  "5 years"] <- "5"
 levels(train$`Years in current job`)[levels(train$`Years in current job`) ==  "6 years"] <- "6"
@@ -22,10 +22,10 @@ levels(train$`Years in current job`)[levels(train$`Years in current job`) ==  "<
 levels(train$`Years in current job`)[levels(train$`Years in current job`) ==  "1 year"] <- "1"
 levels(train$`Years in current job`)[levels(train$`Years in current job`) ==  "2 years"] <- "2"
 levels(train$`Years in current job`)[levels(train$`Years in current job`) ==  "4 years"] <- "4"
-levels(train$`Years in current job`)[levels(train$`Years in current job`) ==  "n/a"] <- "0"
+levels(train$`Years in current job`)[levels(train$`Years in current job`) ==  "n/a"] <- "11"        #Mode of `Years in current job`
 
 #Encoding categorical variables
-train$`Loan Status` <- factor(train$`Loan Status`, labels = c(1,2), levels = c('Fully Paid','Charged Off'))
+train$`Loan Status` <- factor(train$`Loan Status`, labels = c(1,0), levels = c('Fully Paid','Charged Off'))
 train$Term <- factor(train$Term, labels = c(1,2), levels = c('Short Term','Long Term'))
 train$`Home Ownership` <- factor(train$`Home Ownership`, labels = c(1,2,3,4), levels = c('Home Mortgage','Own Home','Rent','HaveMortgage') )
 train$Purpose <- factor(train$Purpose, labels = c(1:17), c('Home Improvements','Debt Consolidation','Buy House','Business Loan','Buy a Car','Educational Expenses','Medical Bills','Take a Trip','major_purchase','moving','vacation','renewable_energy','small_business','wedding','','Other','other'))
@@ -40,6 +40,58 @@ train$`Home Ownership` <- as.integer(train$`Home Ownership`)
 train$Purpose <- as.integer(train$Purpose)
 train$`Years in current job` <- as.integer(train$`Years in current job`)
 
+#Changing Incorrect values in credit score
+train[which(train$`Credit Score`>1000),][,6] <- (train[which(train$`Credit Score`>1000),][,6])/10
+
+#################################### Don't run ##########################################################
+#Outlier Detection
+outlierKD <- function(dt, var) {
+  var_name <- eval(substitute(var),eval(dt))
+  tot <- sum(!is.na(var_name))
+  na1 <- sum(is.na(var_name))
+  m1 <- mean(var_name, na.rm = T)
+  par(mfrow=c(2, 2), oma=c(0,0,3,0))
+  boxplot(var_name, main="With outliers")
+  hist(var_name, main="With outliers", xlab=NA, ylab=NA)
+  outlier <- boxplot.stats(var_name)$out
+  mo <- mean(outlier)
+  var_name <- ifelse(var_name %in% outlier, NA, var_name)
+  boxplot(var_name, main="Without outliers")
+  hist(var_name, main="Without outliers", xlab=NA, ylab=NA)
+  title("Outlier Check", outer=TRUE)
+  na2 <- sum(is.na(var_name))
+  message("Outliers identified: ", na2 - na1, " from ", tot, " observations")
+  message("Proportion (%) of outliers: ", (na2 - na1) / tot*100)
+  message("Mean of the outliers: ", mo)
+  m2 <- mean(var_name, na.rm = T)
+  message("Mean without removing outliers: ", m1)
+  message("Mean if we remove outliers: ", m2)
+  response <- readline(prompt="Do you want to remove outliers and to replace with NA? [yes/no]: ")
+  if(response == "y" | response == "yes"){
+    dt[as.character(substitute(var))] <- invisible(var_name)
+    assign(as.character(as.list(match.call())$dt), dt, envir = .GlobalEnv)
+    message("Outliers successfully removed", "\n")
+    return(invisible(dt))
+  } else{
+    message("Nothing changed", "\n")
+    return(invisible(var_name))
+  }
+}
+
+train_data1 <- as.data.frame(train_data)
+outlierKD(train_data1, `Annual Income`)               #3047 outliers
+outlierKD(train_data1, `Current Loan Amount`)         #9688 outliers
+outlierKD(train_data1, `Monthly Debt`)                #2777 outliers
+outlierKD(train_data1, `Current Credit Balance`)      #4276 Outliers    Decreased range From 0 - 32878968 to 0 - 747498
+outlierKD(train_data1, `Maximum Open Credit`)         #4771 Outliers    Decreased range From 0 - 1539737892 to 0 - 1574980
+
+train$`Annual Income` <- train_data1$`Annual Income`
+train$`Current Loan Amount` <- train_data1$`Current Loan Amount`
+train$`Monthly Debt` <- train_data1$`Monthly Debt`
+train$`Current Credit Balance` <- train_data1$`Current Credit Balance`
+train$`Maximum Open Credit` <- train_data1$`Maximum Open Credit`
+
+################################ Accuracy decreasing when we delete outliers ##################################
 
 #Finding Missing Values
 sort(sapply(train, function(x) { sum(is.na(x)) }), decreasing=TRUE)
@@ -54,10 +106,10 @@ train[,c(6,7,17,18,19)] <- complete(imputed_data, 2)
 ############################## Taking toooo long ############################################################
 
 #imputing Na values using Amelia
-install.packages("Amelia")
+#install.packages("Amelia")
 library(Amelia)
-amelia_fit <- amelia(train[,c(6,7,17,18,19)], m=5, parallel = "multicore")
-train[,c(6,7,17,18,19)] <- amelia_fit$imputations[[2]]
+amelia_fit <- amelia(train[,c(4,6,7,11,16,17)], m=5, parallel = "multicore")
+train[,c(4,6,7,11,16,17)] <- amelia_fit$imputations[[2]]
 ########################## Unable to insert values in bancruptcies :-( #######################################
 
 #Finding mode for the bancruptcies
@@ -69,6 +121,7 @@ mode(train$Bankruptcies)
 
 #Inserting missing values with mode in bancrupcies (here mode is 0)
 train$Bankruptcies[is.na(train$Bankruptcies)] <- 0
+train$`Tax Liens`[is.na(train$`Tax Liens`)] <- 0
 
 # Removing `Loan ID`` and `Customer ID``
 # Removing `Months since last delinquent`` since its has 50% of missing values
@@ -167,17 +220,28 @@ gb_pred <- predict(gb_classifier, newdata = testset[,-16], n.trees = n.trees)
 #kf_pred <- predict(model, newdata = testset[,-16])
 
 pred <- as.data.frame(predict(ann_classifier, newdata = as.h2o(testset[,-16])))
-h2o_pred <- ifelse(pred[,3] >0.5,0,1)
-h2o_pred <- as.integer(h2o_pred)
+#h2o_pred <- ifelse(pred[,3] >0.5,0,1)   #If LoanStatus is 0 and 1
+#h2o_pred <- as.integer(h2o_pred)
 
 #confusion matrix                                                           
-cm_knn <- table(testset$LoanStatus,knn_classifier)    #72% Accuracy
+cm_knn <- table(testset$LoanStatus,knn_classifier)    #72% Accuracy, after deleting outliers 68%
 cm_svm <- table(testset$LoanStatus, svm_pred)         #67% Accuracy
 cm_nb <- table(testset$LoanStatus, nb_pred)           #6490/9910
 cm_dt <- table(testset$LoanStatus, dt_pred)           
-cm_rf <- table(testset$LoanStatus, rf_pred)           
+cm_rf <- table(testset$LoanStatus, rf_pred)           #72% after deleting outliers
 cm_xg <- table(testset$LoanStatus, xg_pred)            
 #cm_gb <- table(testset$LoanStatus, gb_pred)         #Gettign Error:all arguments must have the same length
 #cm_kf <- table(testset$LoanStatus, kf_pred)            
 
-cm_ann <- table(testset$LoanStatus, h2o_pred)         #77% accuracy
+cm_ann <- table(testset$LoanStatus, h2o_pred)         #77% accuracy after deleting outliers 72%
+
+ann_pred <- round(pred)
+ann_pred <- as.integer(ann_pred)
+cm_ann <- table(testset$LoanStatus, ann_pred[[1]])
+
+################### or ################################
+
+library(caret)
+confusionMatrix(testset$LoanStatus, ann_pred[[1]])   #72%
+
+h2o.shutdown()
